d27b7a19d75772e4693adea753f0b9ef7ab6f1f8
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-29 21:09:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def line (array)
+  if katz_deli.length==0
+   puts "The line is currently empty."
+  # elsif
+  # return_string="The line is currently: " 
+  # counter=1
+  # katz_deli.each do |x|
+  #   return_string=return_string.concat(x)
+   
+  # #puts katz_deli.length+1
+  end
+end
